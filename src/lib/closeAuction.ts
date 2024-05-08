78574a74ec9a5da05780fcdd193e311ab0950f59
narpatsingh <44049208+shrinarpat@users.noreply.github.com>
2024-05-08 08:13:49 +0530

    Create closeAuction.ts

---
+++ src/lib/closeAuction.ts
@@ -0,0 +1,42 @@
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
+export async function closeAuction(auction) {
+
+    const params = {
+        TableName: 'AuctionsTable',
+        Key: { id: auction.id },
+        UpdateExpression: 'set #status = :status',
+        ExpressionAttributeValues: {
+            ':status': 'CLOSED'
+        },
+        ExpressionAttributeNames: {
+            '#status': 'status'
+        }
+    };
+
+    await dynamodb.update(params).promise();
+    const {title, seller, highestBid} = auction;
+    const {amount, bidder} = highestBid;
+
+    const notifySeller = sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            subject: 'Your item has been sold!',
+            recipient: seller,
+            body: `Woohoo! Your item "${title}" has been sold for $${amount}.`,
+        })
+    }).promise();
+
+    const notifyBidder = sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            subject: 'You won an auction',
+            recipient: bidder,
+            body: `What a great deal! You got yourself a "${title}" for $${amount}.`,
+        })
+    }).promise()
+
+    return await Promise.all([notifySeller, notifyBidder]);
+}
